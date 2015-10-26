57d3673d62fd73b4be2a50c6aadafb0aa0362d31
Niklas Schröder <scrodde@scrodde-mbp.local>
2015-10-26 22:30:36 +0200

    Default cache duration 10mins. 0 means cache forever not disable cache, oops

--- WeatherPlugin.php
+++ WeatherPlugin.php
@@ -64,7 +64,7 @@ class WeatherPlugin extends BasePlugin
 		return array(
 			'cacheDuration' => array(
 				AttributeType::Number,
-				'default' => 0
+				'default' => 10
 			),
 			'apiKey' => array(
 				AttributeType::String,
